c5f146b415f7059382efe2f028350ca756ed0020
BelNox <mushyiv@gmail.com>
2016-08-14 22:13:43 +0200

    Added my Atom plugin install script

---
+++ atomPackages.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#Adds file specific icons to atom for improved visual grepping. Works with Tree View and Fuzzy Finder and Tabs.
+apm install file-icons;
+
+#DocBlockr is a package for Atom which is designed to make writing documentation faster and easier.
+apm install docblockr;
+
+#Beautifies code
+apm install atom-beautify;
+
+#Minimap
+apm install minimap;
+
+#Shows ToDo's 
+apm install todo-show;
+
+#Feel the power!
+apm install activate-power-mode
+
+#Colour your SQL queries
+apm install qolor
+
+#Google dark guideline colouring
+apm install gl-dark-syntax
+
+#Humble colours
+apm install humble-colors-syntax
+
+#Dark UI theme
+apm install seti-ui
+
+
+
